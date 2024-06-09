a3fbf64bbc34f3db178fb5fa226be3dcdcf2084c
developer-mahin <mdmahin1310@gmail.com>
2024-06-09 17:55:44 +0600

    change localhost url

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -6,7 +6,7 @@ import { tagTypesList } from "./tagTypes";
 export const baseApi = createApi({
   reducerPath: "api",
   baseQuery: axiosBaseQuery({
-    baseUrl: "http://localhost:5000/api/v1",
+    baseUrl: "https://blooddonationserverv2.vercel.app/api/v1",
   }),
   endpoints: () => ({}),
   tagTypes: tagTypesList,
