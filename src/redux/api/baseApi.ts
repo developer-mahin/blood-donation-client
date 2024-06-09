e855423a2318c03fb5591694e4242f9fb19c4ef4
developer-mahin <mdmahin1310@gmail.com>
2024-06-09 18:23:30 +0600

    change to server url

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,12 +1,13 @@
 import { axiosBaseQuery } from "@/helper/axiosBaseQuery";
 import { createApi } from "@reduxjs/toolkit/query/react";
 import { tagTypesList } from "./tagTypes";
+import { baseurl } from "@/constant/URL";
 
 // Define a service using a base URL and expected endpoints
 export const baseApi = createApi({
   reducerPath: "api",
   baseQuery: axiosBaseQuery({
-    baseUrl: "http://localhost:5000/api/v1",
+    baseUrl: `${baseurl}/api/v1`,
   }),
   endpoints: () => ({}),
   tagTypes: tagTypesList,
